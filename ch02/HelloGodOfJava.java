3092d1ae0297ae27a7a856de62b5234d777faabd
dev-hjkim <guswlsapdlf@khu.ac.kr>
2021-10-29 00:24:53 +0900

    ch02 commit

---
+++ ch02/HelloGodOfJava.java
@@ -0,0 +1,9 @@
+public class HelloGodOfJava {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        System.out.println("Test!!");
+        System.out.print("Hello World!");
+        System.out.print("Test!!");
+    }
+
+}
