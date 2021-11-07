e0838b339f3ed2ad6a9174891d3d89b94d61c7ea
dev-hjkim <guswlsapdlf@khu.ac.kr>
2021-11-08 00:45:08 +0900

    ch09 commit

---
+++ ch09/b/control/ControlIf.java
@@ -0,0 +1,16 @@
+package b.control;
+
+public class ControlIf {
+    public static void main(String[] args) {
+        ControlIf controlIf = new ControlIf();
+        controlIf.ifStatement();
+    }
+
+    public void ifStatement() {
+        if(true);
+        if(true) System.out.println("It's true");
+        if(true)
+            System.out.println("It's also true.");
+        if(false) System.out.println("It's false");
+    }
+}
---
+++ ch09/b/variable/VariableTypes.java
@@ -0,0 +1,21 @@
+package b.variable;
+
+public class VariableTypes {
+    int instanceVariable;
+    static int classVariable;
+    public void method(int parameter) {
+        int localVariable;
+    }
+    public void anotherMethod() {
+//        int localVariable;
+        if(true) {
+            int localVariable;
+            if(true) {
+//                int localVariable;
+            }
+        }
+        if(true) {
+            int localVariable;
+        }
+    }
+}
