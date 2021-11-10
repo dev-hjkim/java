f903ea6b28ddef8b83c5003909520ad2216a6d46
dev-hjkim <guswlsapdlf@khu.ac.kr>
2021-11-11 00:48:55 +0900

    ch11 commit

--- ch11/BigDecimal.java
+++ ch11/BigDecimal.java
@@ -1,3 +1,23 @@
 public class BigDecimal {
+    /*
+    1. BigDecimal 용도 :
+     provides operations for arithmetic, scale manipulation, rounding, comparison, hashing, and format conversion
+     consists of arbitrary precision integer unscaled value and a 32-bit integer scale
+
+
+    2. 생성자 개수 : 16개
+
+    3. abs()의 용도 : BigDecimal 객체의 절대값 반환
+
+    4. 현재 값에서 특정 값을 빼는 연산을 수행하는 메소드 : BigDecimal subtract(BigDecimal subtrahend)
+
+    5. 나누기와 관련된 메소드 :
+    	BigDecimal divide(BigDecimal divisor)
+    	BigDecimal divide(BigDecimal divisor, int roundingMode)
+    	BigDecimal divide(BigDecimal divisor, int scale, int roundingMode)
+    	BigDecimal divide(BigDecimal divisor, int scale, RoundingMode roundingMode)
+    	BigDecimal divide(BigDecimal divisor, MathContext mc)
+    	BigDecimal divide(BigDecimal divisor, RoundingMode roundingMode)
+     */
 
 }
