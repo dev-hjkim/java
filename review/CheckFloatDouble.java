b7fa933f3162aa73ce388d881fc64e413dc78997
dev-hjkim <guswlsapdlf@khu.ac.kr>
2021-11-16 00:43:33 +0900

    review commit

---
+++ review/CheckFloatDouble.java
@@ -0,0 +1,9 @@
+public class CheckFloatDouble {
+    public static void main(String[] args) {
+        float f1 = 0.1f;
+        double d1 = 0.1;
+
+        System.out.format("The value of float 0.1 is %.23f\n", f1);
+        System.out.format("The value of double 0.1 is %.23f\n", d1);
+    }
+}
