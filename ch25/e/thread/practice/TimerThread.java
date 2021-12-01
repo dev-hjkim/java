4d70ff2bb54b0e92308ce7031503e5ce424a419f
dev-hjkim <guswlsapdlf@khu.ac.kr>
2021-12-02 00:55:52 +0900

    ch25 commit

--- ch25/e/thread/practice/TimerThread.java
+++ ch25/e/thread/practice/TimerThread.java
@@ -6,21 +6,45 @@ public class TimerThread extends Thread{
     public void run() {
         TimerThread thread = new TimerThread();
         thread.printCurrentTime();
+        thread.printCurrentTimeMillis();
     }
 
     public void printCurrentTime() {
         int loop = 0;
-        while(loop < 10) {
-            Date cur = new Date();
-            System.out.println(cur.toString());
-            System.currentTimeMillis();
-            try {
+        try {
+            while(loop < 10) {
                 Thread.sleep(1000);
+                long currentTime = System.currentTimeMillis();
+                System.out.println(new Date(currentTime)+" "+currentTime);
                 loop++;
-            } catch (Exception ex) {
-                ex.printStackTrace();
             }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
+    public void reduceTimeGap() {
+        long currentTime=System.currentTimeMillis();
+        long timeMod=currentTime%1000;
+        try {
+            Thread.sleep(1000-timeMod);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printCurrentTimeMillis() {
+        try {
+            int count=0;
+            while(count<10) {
+                long current=System.currentTimeMillis();
+                System.out.println(new Date(current)+" "+current);
+                Thread.sleep(900);
+                reduceTimeGap();
+                count++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
