4de4a4e9b0747e32b4341d7de1c6ff3ea9ad3436
dev-hjkim <guswlsapdlf@khu.ac.kr>
2021-12-04 22:55:37 +0900

    ch26 question5 answer

--- ch26/e/io/practice/FileSizeSummary.java
+++ ch26/e/io/practice/FileSizeSummary.java
@@ -1,13 +1,14 @@
 package e.io.practice;
 
 import java.io.File;
+import java.text.DecimalFormat;
 
 public class FileSizeSummary {
     public static void main(String[] args) {
         FileSizeSummary sample = new FileSizeSummary();
         String path = "D:\\java";
         long sum = sample.printFileSize(path);
-        System.out.println(path + "'s total size=" + sum);
+        System.out.println(path + "'s total size=" + sample.convertFileLength(sum));
     }
 
     public long printFileSize(String dirName) {
@@ -20,16 +21,36 @@ public class FileSizeSummary {
                 if (file.isFile()) {
                     String tempFileName = file.getAbsolutePath();
                     long fileLength = file.length();
-                    System.out.println(tempFileName + "=" + fileLength);
+                    System.out.println(tempFileName + "=" + convertFileLength(fileLength));
                     sum += fileLength;
                 } else {
                     String tempDirName = file.getAbsolutePath();
                     long fileLength = printFileSize(tempDirName);
-                    System.out.println("[" + tempDirName + "]=" + fileLength);
+                    System.out.println("[" + tempDirName + "]=" + convertFileLength(fileLength));
                     sum += fileLength;
                 }
             }
         }
         return sum;
+    }
+
+    private String convertFileLength(long fileLength) {
+        String sizeFormat;
+        DecimalFormat df = new DecimalFormat("0.##");
+        if (fileLength <= 1024) {
+            String fileSize = df.format(1.0 * fileLength);
+            sizeFormat = fileSize + " b";
+        } else if (1024 < fileLength && fileLength <= 1024*1024) {
+            String fileSize = df.format(1.0 * fileLength / 1024);
+            sizeFormat = fileSize + " kb";
+        } else if (1024*1024 < fileLength && fileLength <= 1024*1024*1024) {
+            String fileSize = df.format(1.0 * fileLength / (1024*1024));
+            sizeFormat = fileSize + " mb";
+        } else {
+            String fileSize = df.format(1.0 * fileLength / (1024*1024*1024));
+            sizeFormat = fileSize + " gb";
+        }
+
+        return sizeFormat;
     }
 }
