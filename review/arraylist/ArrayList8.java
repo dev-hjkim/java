1a1651aa00ede3673c5ed14ec9def214ab4ee511
dev-hjkim <guswlsapdlf@khu.ac.kr>
2021-12-08 01:41:41 +0900

    add hugeCapacity method(jdk8)

--- review/arraylist/ArrayList8.java
+++ review/arraylist/ArrayList8.java
@@ -116,4 +116,12 @@ public class ArrayList<E> extends AbstractList<E>
         // minCapacity is usually close to size, so this is a win:
         elementData = Arrays.copyOf(elementData, newCapacity);
     }
+
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0) // overflow
+            throw new OutOfMemoryError();
+        return (minCapacity > MAX_ARRAY_SIZE) ?
+                Integer.MAX_VALUE :
+                MAX_ARRAY_SIZE;
+    }
 }
