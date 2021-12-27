05d20ef0d82a2bb2e243fc8839aa39eed1dd20c3
dev-hjkim <dev--test@naver.com>
2021-12-28 02:43:21 +0900

    stream example code for velog

---
+++ review/stream/Stream.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Stream {
+    public static void main(String[] args) {
+        List<Integer> lst = new ArrayList<>();
+        for (int i=1; i<11; i++) {
+            lst.add(i);
+        }
+
+        int res = lst.stream().mapToInt(i -> i.intValue()).sum();
+        System.out.println(res);
+    }
+}
